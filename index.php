817e01c4ab64df6eb09519cfe2194c9a3d70a44a
Andino Martínez Nelson David <ndandino@espe.edu.ec>
2026-05-21 10:43:17 -0500

    views

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
+$requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?: '/';
+$assetPath = __DIR__ . $requestPath;
+
+$extension = strtolower(pathinfo($assetPath, PATHINFO_EXTENSION));
+$contentTypeMap = [
+    'css' => 'text/css; charset=UTF-8',
+    'js' => 'application/javascript; charset=UTF-8',
+    'json' => 'application/json; charset=UTF-8',
+    'svg' => 'image/svg+xml',
+    'png' => 'image/png',
+    'jpg' => 'image/jpeg',
+    'jpeg' => 'image/jpeg',
+    'gif' => 'image/gif',
+    'ico' => 'image/x-icon',
+];
+
+if (is_file($assetPath) && isset($contentTypeMap[$extension])) {
+    header('Content-Type: ' . $contentTypeMap[$extension]);
+    readfile($assetPath);
+    exit;
+}
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use App\Controller\ProductController;
